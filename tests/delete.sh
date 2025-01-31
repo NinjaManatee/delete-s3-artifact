b6afefef8dde63cf795f80c7ffb83ccb0914051c
NinjaManatee <andy.gardner@interop.io>
2025-01-31 04:45:28 -0500

    Added more test runs and loading of env var

--- tests/delete.sh
+++ tests/delete.sh
@@ -4,24 +4,67 @@
 #
 # Usage: delete.sh
 
+#region get script directory
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+#endregion
+
 #region initialize common environment variables
 export RUNNER_OS="Windows"
 export GITHUB_REPOSITORY="foo/bar"
 export GITHUB_RUN_ID="1"
 export S3_ARTIFACTS_BUCKET="this-is-an-s3-bucket-name"
-export DRY_RUN=true
 
 # variables needed, but are usually defined by the GitHub runner
 export RUNNER_TEMP="$TEMP"
 export RUNNER_DEBUG=true
 export GITHUB_OUTPUT=/dev/null
 export GITHUB_STEP_SUMMARY=/dev/null
+
+if [[ -f "$DIR/../env.sh" ]]; then
+    echo "env file found, executing"
+    source "$dir/../env.sh"
+else
+    echo "Executing dry run"
+    DRY_RUN="true"
+fi
+
 #endregion
 
-#region delete single file
+#region delete single file with glob
+#region set up environment variables
+echo "Initializing variables"
+export INPUT_NAME="tempArchive*"
+export INPUT_USE_GLOB="true"
+export INPUT_FAIL_ON_ERROR="true"
+#endregion
+
+#region run main script
+echo "Running main.sh"
+source "$DIR/../scripts/main.sh"
+#endregion
+#endregion
+
+#region delete single file without glob
 #region set up environment variables
 echo "Initializing variables"
 export INPUT_NAME="tempArchiveName"
+export INPUT_USE_GLOB="false"
+export INPUT_FAIL_ON_ERROR="true"
+#endregion
+
+#region run main script
+echo "Running main.sh"
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/../scripts/main.sh"
+#endregion
+#endregion
+
+#region delete array of files with glob
+#region set up environment variables
+echo "Initializing variables"
+export INPUT_NAME=firstFile* secondFile* thirdFile*
 export INPUT_USE_GLOB="true"
 export INPUT_FAIL_ON_ERROR="true"
 #endregion
@@ -34,11 +77,11 @@ source "$DIR/../scripts/main.sh"
 #endregion
 #endregion
 
-#region delete array of files
+#region delete array of files without glob
 #region set up environment variables
 echo "Initializing variables"
 export INPUT_NAME="tempArchiveName1 tempArchiveName2 tempArchiveName3"
-export INPUT_USE_GLOB="true"
+export INPUT_USE_GLOB="false"
 export INPUT_FAIL_ON_ERROR="true"
 #endregion
 
